52b08673208760c1cc7c4c18dbd726b38496a1f7
Angel Alonso Menendez <angel.alonso@students.salleurl.edu>
2019-03-01 00:56:52 +0100

    YAML Mapping

--- src/MyApp/Component/Product/Domain/Product.php
+++ src/MyApp/Component/Product/Domain/Product.php
@@ -4,38 +4,22 @@ namespace MyApp\Component\Product\Domain;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="product")
- */
+
 class Product
 {
-    /**
-     * @ORM\Column(type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
+
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=100)
-     */
+
     private $name;
 
-    /**
-     * @ORM\Column(type="decimal", scale=2)
-     */
+
     private $price;
 
-    /**
-     * @ORM\Column(type="text")
-     */
+
     private $description;
     
-    /**
-     * @ORM\ManyToOne(targetEntity="Owner", inversedBy="products", cascade={"persist"})
-     * @ORM\JoinColumn(name="owner_id", referencedColumnName="id")
-     */
+
     private $owner;
 
     public function __construct(string $name, float $price, string $description, Owner $owner)
@@ -46,74 +30,56 @@ class Product
         $this->owner = $owner;
     }
 
-    /**
-     * @return mixed
-     */
+
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * @return mixed
-     */
+
     public function getName()
     {
         return $this->name;
     }
 
-    /**
-     * @param mixed $name
-     */
+
     public function setName($name)
     {
         $this->name = $name;
 
     }
 
-    /**
-     * @return mixed
-     */
+
     public function getPrice()
     {
         return $this->price;
     }
 
-    /**
-     * @param mixed $price
-     */
+
     public function setPrice($price)
     {
         $this->price = $price;
     }
 
-    /**
-     * @return mixed
-     */
+
     public function getDescription()
     {
         return $this->description;
     }
 
-    /**
-     * @param mixed $description
-     */
+
     public function setDescription($description)
     {
         $this->description = $description;
     }
 
-    /**
-     * @return mixed
-     */
+
     public function getOwner()
     {
         return $this->owner;
     }
 
-    /**
-     * @param mixed $owner
-     */
+
     public function setOwner($owner)
     {
         $this->owner = $owner;
